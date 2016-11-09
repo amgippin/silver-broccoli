71a1fae2dba6430404aea57bbd974ff66ddc95e1
amgippin <amgippin@gmail.com>
2016-11-08 22:56:43 -0800

    Tour of Heros Tutorial Chapter 1 & 2

--- angular2/angular-tour-of-heroes/app/app.component.ts
+++ angular2/angular-tour-of-heroes/app/app.component.ts
@@ -1,6 +1,42 @@
 import { Component } from '@angular/core';
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>Hello Angular2!</h1>'
-})
-export class AppComponent { }
+	selector: 'my-app',
+	/*First time in my life I used a back tick...*/
+	template: `
+		<h1>{{title}}</h1>
+		<h2>{{hero.name}} details!</h2>
+		
+		<label>id: </label>{{hero.id}}
+		<div>		
+			<!--Two way binding-->
+			Two way binding
+			<input [(ngModel)]="hero.name" placeholder="name">
+		</div>
+		<div>
+			<!--One way binding-->
+			One way binding
+			<input value="{{hero.name}}" placeholder="name">
+		</div>
+	`
+});
+
+/*Why is this one equal and the others colon?*/
+export class AppComponent { 
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
+}
+
+
+
+
+
+
