89506cbe960eebfe589743d080f2a217a8d6118e
amgippin <amgippin@gmail.com>
2016-11-09 21:17:29 -0800

    Tour of Heroes Chapter 1
    Tour of Heros Chapter1

--- angular2/angular-tour-of-heroes/app/app.component.ts
+++ angular2/angular-tour-of-heroes/app/app.component.ts
@@ -1,42 +1,33 @@
 import { Component } from '@angular/core';
 
+@Component({
+	selector: 'my-app',
+	template:`
+		<h1>{{title}}</h1>
+		<h2>{{hero.name}} details!</h2>
+		<div><label>id: </label>{{hero.id}}</div>
+		<div>
+			<!--one way binding-->
+			<label>One way binding: </label>
+			<input value="{{hero.name}}" placeholder="name">
+		</div>
+		<div>
+			<!--two way binding-->
+			<label>Two way binding: </label>
+			<input [(ngModel)]="hero.name" placeholder="name">
+		</div>
+	`
+})
+
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+	  id: 1,
+	  name: 'Windstorm'
+	};
+}
+
 export class Hero {
 	id: number;
 	name: string;
 }
-
-@Component({
-	selector: 'my-app',
-	/*First time in my life I used a back tick...*/
-	template: `
-		<h1>{{title}}</h1>
-		<h2>{{hero.name}} details!</h2>
-		
-		<label>id: </label>{{hero.id}}
-		<div>		
-			<!--Two way binding-->
-			Two way binding
-			<input [(ngModel)]="hero.name" placeholder="name">
-		</div>
-		<div>
-			<!--One way binding-->
-			One way binding
-			<input value="{{hero.name}}" placeholder="name">
-		</div>
-	`
-});
-
-/*Why is this one equal and the others colon?*/
-export class AppComponent { 
-	title = 'Tour of Heroes';
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
-}
-
-
-
-
-
-
